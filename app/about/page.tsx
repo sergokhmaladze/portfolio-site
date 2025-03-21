6be7ddaaf85dff77bc91cccbf66dfae9c78622de
sergo <sergo.khmaladze@gmail.com>
2025-03-21 20:44:52 +0400

    change some text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,27 +4,27 @@ import styles from './about.module.css';
 
 function About(): React.JSX.Element {
   return (
-  <div className={styles.container}>
-    <div className={styles.aboutMe}>
+    <div className={styles.container}>
+      <div className={styles.aboutMe}>
         <h1>ჩემს შესახებ</h1>
-        <p>აქ ნახავთ მეტ ინფორმაციას ჩემს შესახებ,
-            რას ვაკეთებ და ჩემს ამჟამინდელ უნარებს
-            ძირითადად პროგრამირებისა და ტექნოლოგიების კუთხით</p>
-        
-    </div>
-    <div className={styles.getToKnowMe}>
-        <h1>გამიცანი!</h1>
-        <p>მე ვარ სერგო ხმალაძე, დამწყები Front End დეველოპერი,
-            ღია ვარ სამუშაო შესაძლებლობებისთვის, სადაც შემიძლია
-            წვლილი შევიტანო, ვისწავლო და გავიზარდო. თუ გაქვთ
-            უნარებსა და გამოცდილებას, ნუ მოგერიდებათ დამიკავშირდეთ.
+        <p className={styles.aboutMeText}>
+          აქ ნახავთ მეტ ინფორმაციას ჩემს შესახებ, რას ვაკეთებ და ჩემს ამჟამინდელ
+          უნარებს ძირითადად პროგრამირებისა და ტექნოლოგიების კუთხით
         </p>
+      </div>
+      <div className={styles.getToKnowMe}>
+        <h1>გამიცანი!</h1>
+        <p className={styles.aboutMeText}>
+          მე ვარ სერგო ხმალაძე, დამწყები Front End დეველოპერი, ღია ვარ სამუშაო
+          შესაძლებლობებისთვის, სადაც შემიძლია წვლილი შევიტანო, ვისწავლო და
+          გავიზარდო.
+        </p>
+      </div>
+      <div>
+        <SkillsApp></SkillsApp>
+      </div>
     </div>
-    <div>
-      <SkillsApp></SkillsApp>
-    </div>
-  </div>
-  )
+  );
 }
 
 export default About;
--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,39 @@ import styles from './home.module.css';
 import Link from 'next/link';
 import projects from '../data/projects.json';
 
-
 export default function Home(): React.JSX.Element {
   return (
     <main>
       <div className={styles.container}>
         <h1 className={styles.whoAmIheader}>გამარჯობა,მე ვარ სერგო ხმალაძე</h1>
-        <p>შედეგზე ორიენტირებული ვებ დეველოპერი, 
-           რომელიც აკეთებს და მართავს ვებსაიტებს და ვებ აპლიკაციებს, 
-           რაც იწვევს მთლიანი პროდუქტის წარმატებას</p>
+        <p>
+          დამწყები Front End დეველოპერი, ღია ვარ სამუშაო შესაძლებლობებისთვის,
+          სადაც შემიძლია წვლილი შევიტანო, ვისწავლო და გავიზარდო
+        </p>
       </div>
-      
+
       <div className={styles.container__projects}>
         <h1 className={styles.container__title}>My Projects</h1>
         <div className={styles.projects__grid}>
-        {projects.map((project) => (
-          <div className={styles.project} key={project.id}>
-            <div className={styles.project__content}>
-              <Link className={styles.project__link} href={`/projects/${project.slug}` } >
-                {project.title}
-              </Link>
-              <img className={styles.project__image} src={project.image} width={250} />
+          {projects.map((project) => (
+            <div className={styles.project} key={project.id}>
+              <div className={styles.project__content}>
+                <Link
+                  className={styles.project__link}
+                  href={`/projects/${project.slug}`}
+                >
+                  {project.title}
+                </Link>
+                <img
+                  className={styles.project__image}
+                  src={project.image}
+                  width={250}
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         </div>
       </div>
     </main>
   );
 }
-
-
-
-
-
-          
